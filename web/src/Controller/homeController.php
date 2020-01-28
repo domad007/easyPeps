d4142cf72f0f5876ed97cfb51b9942e8d7a58da7
domad007 <44199654+domad007@users.noreply.github.com>
2020-01-28 12:36:32 +0100

    Remplissange des liens dans la navbar

--- web/src/Controller/homeController.php
+++ web/src/Controller/homeController.php
@@ -19,4 +19,133 @@ class homeController extends AbstractController {
         );
         
     }
+
+    /**
+     * @Route("/introduction", name="introduction")
+     */
+
+    public function introduction(){
+         return $this->render(
+             '/contenu/introduction.html.twig'
+         );
+    }
+
+    /**
+     * @Route("/genese", name="genese")
+     */
+
+    public function genese(){
+        return $this->render(
+            '/contenu/genese.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/qui-suis-je", name="qui")
+     */
+    public function quiSuisJe(){
+        return $this->render(
+            '/contenu/quiSuisJe.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/objectifs", name="objectifs")
+     */
+    public function objectifs(){
+        return $this->render(
+            '/contenu/objectif.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/journalClasse", name="journalClasse")
+     */
+    public function journalClasse(){
+        return $this->render(
+            '/contenu/journal.html.twig'
+        );    
+    }
+
+    /**
+     * @Route("/agenda", name="agenda")
+     */
+    public function agenda(){
+        return $this->render(
+            '/contenu/agenda.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/cahierCotes", name="cahier")
+     */
+    public function cahier(){
+        return $this->render(
+            '/contenu/cahierCotes.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/planning", name="planning")
+     */
+    public function planning(){
+        return $this->render(
+            '/contenu/planning.html.twig'
+        );
+    }
+
+        /**
+     * @Route("/parametres", name="parametres")
+     */
+    public function parametres(){
+        return $this->render(
+            '/contenu/parametres.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/vma", name="vma")
+     */
+    public function vma(){
+        return $this->render(
+            '/contenu/vma.html.twig'
+        );
+    }
+
+        /**
+     * @Route("/course", name="course")
+     */
+    public function course(){
+        return $this->render(
+            '/contenu/course.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/evaluation", name="evaluation")
+     */
+    public function evaluation(){
+        return $this->render(
+            '/contenu/evaluation.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/application", name="application")
+     */
+    public function application(){
+        return $this->render(
+            '/contenu/appli.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/formation", name="formation")
+     */
+    public function formation(){
+        return $this->render(
+            '/contenu/formation.html.twig'
+        );
+    }
+
 }
