6df74f5cbfc5cda8f73c8070ed784ce9b14ec64e
domad007 <44199654+domad007@users.noreply.github.com>
2020-03-15 13:10:21 +0100

    accès au journal de classe fini

--- web/src/Controller/journalController.php
+++ web/src/Controller/journalController.php
@@ -7,17 +7,48 @@ use App\Entity\Cours;
 use App\Entity\Eleve;
 use App\Entity\Classe;
 use App\Entity\CoursGroupe;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class journalController extends AbstractController {
 
     /**
-     * @Route("/journalDeCalsse/{idGroup}", name="journal_de_classe")
+     * @Route("/journalDeClasse", name="journal")
      */
-    public function journalDeCalsse($idGroup){
+    public function journal(UserInterface $user){
+
+        $manager = $this->getDoctrine()->getManager();
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('ecole', 'ecole');
+        $rsm->addScalarResult('groups_id', 'groups_id');
+        $rsm->addScalarResult('groupes', 'groupes');
+
+        $groupsSql = "select ecole.nom_ecole as ecole, groups_id, GROUP_CONCAT(nom_classe SEPARATOR '/') as groupes 
+        from classe
+        join ecole on classe.ecole_id = ecole.id
+        where groups_id is not null and professeur_id = ?
+        group by groups_id";
+
+        $getGroups = $manager->createNativeQuery($groupsSql, $rsm);
+        $getGroups->setParameter(1, $user->getId());
+
+        $groups = $getGroups->getResult();
+
+        return $this->render(
+            'journalDeClasse/journalChoix.html.twig', 
+            [
+                'groups' => $groups
+            ]
+        );
+    }
+    /**
+     * @Route("/journalDeClasse/{idGroup}", name="journal_de_classe")
+     */
+    public function journalDeCalsse(UserInterface $user, $idGroup){
         $manager = $this->getDoctrine()->getManager();
         $getCours = $manager
         ->getRepository(Cours::class)
@@ -26,6 +57,22 @@ class journalController extends AbstractController {
         $group = $manager
         ->getRepository(Classe::class)
         ->findByGroups($idGroup);
+
+        $rsm = new ResultSetMapping();
+        $rsm->addScalarResult('ecole', 'ecole');
+        $rsm->addScalarResult('groups_id', 'groups_id');
+        $rsm->addScalarResult('groupes', 'groupes');
+
+        $groupsSql = "select ecole.nom_ecole as ecole, groups_id, GROUP_CONCAT(nom_classe SEPARATOR '/') as groupes 
+        from classe
+        join ecole on classe.ecole_id = ecole.id
+        where groups_id is not null and professeur_id = ?
+        group by groups_id";
+
+        $getGroups = $manager->createNativeQuery($groupsSql, $rsm);
+        $getGroups->setParameter(1, $user->getId());
+
+        $groups = $getGroups->getResult();
 
         foreach($group as $key => $value){           
             $eleves [] = $manager
@@ -37,12 +84,18 @@ class journalController extends AbstractController {
             );
         }
 
-        //dump($getCours);
+        $getCoursGroupe = $manager
+        ->getRepository(CoursGroupe::class)
+        ->findBycoursId($getCours);
+
         return $this->render(
             'journalDeClasse/journal.html.twig', 
             [
                 'cours' => $getCours,
-                'eleves' => $eleves
+                'eleves' => $eleves,
+                'coursGroupe' => $getCoursGroupe,
+                'groups' => $groups,
+                'ecole' => $group
             ]
         );
     }
